06d4b7d8f9fe8fbbd97a11ac0b839064bcc156d7
Rolf Strijdhorst <r.strijdhorst@cohesion.nl>
2023-01-28 16:24:10 +0100

    chore(statuscol): inprove clickhandlers

--- after/plugin/statuscol.lua
+++ after/plugin/statuscol.lua
@@ -2,6 +2,7 @@ local statuscol_nvim_status_ok, statuscol_nvim = pcall(require, "statuscol")
 if not statuscol_nvim_status_ok then
 	return
 end
+local builtin = require("statuscol.builtin")
 statuscol_nvim.setup({
 	separator = " ",
 	thousands = false,
@@ -14,16 +15,17 @@ statuscol_nvim.setup({
 	FoldPlus = false,
 	FoldMinus = false,
 	FoldEmpty = false,
+	-- click actions
 	DapBreakpointRejected = false,
-	DapBreakpoint = false,
-	DapBreakpointCondition = false,
-	DiagnosticSignError = false,
-	DiagnosticSignHint = false,
-	DiagnosticSignInfo = false,
-	DiagnosticSignWarn = false,
+	DapBreakpoint = builtin.toggle_breakpoint,
+	DapBreakpointCondition = builtin.toggle_breakpoint,
+	DiagnosticSignError = builtin.diagnostic_click,
+	DiagnosticSignHint = builtin.diagnostic_click,
+	DiagnosticSignInfo = builtin.diagnostic_click,
+	DiagnosticSignWarn = builtin.diagnostic_click,
 	GitSignsTopdelete = false,
 	GitSignsUntracked = false,
-	GitSignsAdd = false,
+	GitSignsAdd = builtin.gitsigns_click,
 	GitSignsChangedelete = false,
 	GitSignsDelete = false,
 })
