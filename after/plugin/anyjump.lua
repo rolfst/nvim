55a696ed356366b2967bc626d8f2b9d255c8e8db
Rolf Strijdhorst <r.strijdhorst@cohesion.nl>
2023-01-28 18:11:20 +0100

    chore: more logical key for anyjump

--- after/plugin/anyjump.lua
+++ after/plugin/anyjump.lua
@@ -1,4 +1,4 @@
 vim.g.any_jump_disable_default_keybindings = 1
 vim.g.any_jump_list_numbers = 1
-vim.keymap.set("n", "<A-u>", ":AnyJump<CR>", { noremap = true, silent = true, desc = "AnyJump" })
-vim.keymap.set("v", "<A-u>", ":AnyJumpVisual<CR>", { noremap = true, silent = true, desc = "AnyJumpVisual" })
+vim.keymap.set("n", "<A-j>", ":AnyJump<CR>", { noremap = true, silent = true, desc = "AnyJump" })
+vim.keymap.set("v", "<A-j>", ":AnyJumpVisual<CR>", { noremap = true, silent = true, desc = "AnyJumpVisual" })
