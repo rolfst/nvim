21b051ee7213d9d9d8d0ff86c42320d6e13b2090
Rolf Strijdhorst <rolfst@gmail.com>
2023-03-14 19:33:55 +0100

    fix: duplicate usage <c-e>

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -1,12 +1,12 @@
 local mark = require("harpoon.mark")
 local ui = require("harpoon.ui")
 
-vim.keymap.set("n", "<c-e>a", mark.add_file, { desc = "Att harpoon marker" })
-vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu, { desc = "Toggle harpoon listing" })
+vim.keymap.set("n", "<space>ea", mark.add_file, { desc = "Att harpoon marker" })
+vim.keymap.set("n", "<space>e", ui.toggle_quick_menu, { desc = "Toggle harpoon listing" })
 
-vim.keymap.set("n", "<C-e>n", function()
-	ui.nav_next()
+vim.keymap.set("n", "<space>en", function()
+    ui.nav_next()
 end, { desc = "harpoon next" })
-vim.keymap.set("n", "<C-e>p", function()
-	ui.nav_prev()
+vim.keymap.set("n", "<space>ep", function()
+    ui.nav_prev()
 end, { desc = "harpoon previous" })
