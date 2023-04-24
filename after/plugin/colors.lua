ed85d905054f84e26dee97a11478479f1990e88f
rolfst <rolfst@gmail.com>
2023-04-24 23:03:45 +0200

    setup colorizer

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,11 +1,13 @@
 local lush = require("lush")
-local nvim_treeclimber_status_ok, nvim_treeclimber = pcall(require, "nvim-treeclimber")
+local nvim_treeclimber_status_ok, nvim_treeclimber =
+    pcall(require, "nvim-treeclimber")
 if not nvim_treeclimber_status_ok then
     return
 end
 -- Change if you don't have Lush installed
 --
-local theme_colors = _G.NVIM_SETTINGS.colorschemes.colors[_G.NVIM_SETTINGS.colorschemes.theme]
+local theme_colors =
+    _G.NVIM_SETTINGS.colorschemes.colors[_G.NVIM_SETTINGS.colorschemes.theme]
 local color = require("nvim-treeclimber.hi")
 -- print(vim.inspect(color))
 local bg = lush.hsl(theme_colors.bg_01)
@@ -14,13 +16,25 @@ local dim = bg.mix(fg, 20)
 
 vim.api.nvim_set_hl(0, "TreeClimberHighlight", { background = dim.hex })
 
-vim.api.nvim_set_hl(0, "TreeClimberSiblingBoundary", { background = color.terminal_color_5.hex })
+vim.api.nvim_set_hl(
+    0,
+    "TreeClimberSiblingBoundary",
+    { background = color.terminal_color_5.hex }
+)
 
-vim.api.nvim_set_hl(0, "TreeClimberSibling", { background = color.terminal_color_5.mix(bg, 40).hex, bold = true })
+vim.api.nvim_set_hl(
+    0,
+    "TreeClimberSibling",
+    { background = color.terminal_color_5.mix(bg, 40).hex, bold = true }
+)
 
 vim.api.nvim_set_hl(0, "TreeClimberParent", { background = bg.mix(fg, 2).hex })
 
-vim.api.nvim_set_hl(0, "TreeClimberParentStart", { background = color.terminal_color_4.mix(bg, 10).hex, bold = true })
+vim.api.nvim_set_hl(
+    0,
+    "TreeClimberParentStart",
+    { background = color.terminal_color_4.mix(bg, 10).hex, bold = true }
+)
 
 nvim_treeclimber.setup()
 function ColorScheme(color)
@@ -32,3 +46,7 @@ function ColorScheme(color)
 end
 
 ColorScheme()
+--------------
+-- colorize r
+local colorizer = pcall(require, "colorizer")
+colorizer.setup()
