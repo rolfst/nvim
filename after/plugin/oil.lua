b353ec8dcd9ba27c9ad3e5d8c95ecfd618f4662f
rolfst <rolfst@gmail.com>
2023-10-17 10:34:25 +0200

    fix(Oil): typo

--- after/plugin/oil.lua
+++ after/plugin/oil.lua
@@ -3,3 +3,9 @@ if not oil_status_ok then
     return
 end
 oil.setup({})
+vim.keymap.set(
+    "n",
+    "-",
+    "<CMD>Oil --float<CR>",
+    { noremap = true, description = "file browser", silent = true }
+)
