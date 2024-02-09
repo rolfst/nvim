b085ce8c611387a159d970eb40fb197560d9da9e
rolfst <rolfst@gmail.com>
2024-02-09 15:25:13 +0100

    fix(treesitter): treesitter no longer managed by vim but nixos

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,6 +1,6 @@
 require("nvim-treesitter.configs").setup({
     -- A list of parser names, or "all"
-    ensure_installed = { "vimdoc", "typescript", "lua", "rust", "python", "javascript" },
+    -- ensure_installed = { "vimdoc", "typescript", "lua", "rust", "python", "javascript" },
 
     -- Install parsers synchronously (only applied to `ensure_installed`)
     sync_install = false,
