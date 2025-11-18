cfb283701949a0637ffaffa9db9dffa75d2fa880
rolfst <rolfst@gmail.com>
2025-11-18 13:29:22 +0100

    feat: add snacks debugging

--- lua/rolfst/init.lua
+++ lua/rolfst/init.lua
@@ -8,9 +8,28 @@ else
     global["diagnostics"] = {}
     global["diagnostics"]["path"] = vim.fn.getcwd()
     global["diagnostics"]["method"] = "global"
-    _G.NVIM_SETTINGS = funcs.read_file(global.nvim_path .. "/config/config.json")
+    _G.NVIM_SETTINGS =
+        funcs.read_file(global.nvim_path .. "/config/config.json")
 end
 require("rolfst.options")
 require("rolfst.remap")
 require("rolfst.plugins")
 require("rolfst.autocommands")
+
+local status_ok, Snacks = pcall(require, "snacks")
+if not status_ok then
+    return
+end
+_G.dd = function(...)
+    Snacks.debug.inspect(...)
+end
+_G.bt = function()
+    Snacks.debug.backtrace()
+end
+if vim.fn.has("nvim-0.11") == 1 then
+    vim._print = function(_, ...)
+        dd(...)
+    end
+else
+    vim.print = dd
+end
