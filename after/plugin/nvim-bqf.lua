02a91dc0781620f3f36061625df57f1e69073424
rolfst <rolfst@gmail.com>
2025-11-18 13:31:18 +0100

    feat: add quicker for quicklist editting (fails)

--- after/plugin/nvim-bqf.lua
+++ after/plugin/nvim-bqf.lua
@@ -1,9 +1,34 @@
 local bqf_status_ok, bqf = pcall(require, "bqf")
-if not bqf_status_ok then
+-- if not bqf_status_ok then
+--     return
+-- end
+-- bqf.setup({
+--     preview = {
+--         border_chars = {
+--             "│",
+--             "│",
+--             "─",
+--             "─",
+--             "┌",
+--             "┐",
+--             "└",
+--             "┘",
+--             "█",
+--         },
+--     },
+-- })
+local status_ok, quicker = pcall(require, "quicker")
+if not status_ok then
     return
 end
-bqf.setup({
-    preview = {
-        border_chars = { "│", "│", "─", "─", "┌", "┐", "└", "┘", "█" },
-    },
+quicker.setup()
+vim.keymap.set("n", "<leader>q", function()
+    quicker.toggle()
+end, {
+    desc = "Toggle quickfix",
+})
+vim.keymap.set("n", "<leader>l", function()
+    quicker.toggle({ loclist = true })
+end, {
+    desc = "Toggle loclist",
 })
--- lua/rolfst/plugins.lua
+++ lua/rolfst/plugins.lua
@@ -73,7 +73,7 @@ require("lazy").setup({
     { "ThePrimeagen/harpoon" },
     { "rgroli/other.nvim" },
     -- { "mbbill/undotree" },
-    { "XXiaoA/atone.nvim", cmd = "Atone" },
+    { "XXiaoA/atone.nvim",         cmd = "Atone" },
     {
         "neovim/nvim-lspconfig",
     },
@@ -112,7 +112,7 @@ require("lazy").setup({
         version = "1.*",
         build = "cargo build --release",
         dependencies = {
-            { "L3MON4D3/LuaSnip", version = "v2.*" },
+            { "L3MON4D3/LuaSnip",            version = "v2.*" },
             { "rafamadriz/friendly-snippets" },
         },
         sources = {
@@ -206,13 +206,16 @@ require("lazy").setup({
     },
     { "echasnovski/mini.nvim", version = false },
     { "echasnovski/mini.move", version = false },
-    { "echasnovski/mini.ai", version = false },
+    { "echasnovski/mini.ai",   version = false },
     { "echasnovski/mini.diff", version = false },
     {
         "HakonHarnes/img-clip.nvim",
     },
 
-    { "Dkendal/nvim-treeclimber", dependencies = { "rktjmp/lush.nvim" } },
+    {
+        "Dkendal/nvim-treeclimber",
+        dependencies = { "rktjmp/lush.nvim" },
+    },
     {
         "windwp/nvim-autopairs",
         dependencies = {
@@ -249,11 +252,15 @@ require("lazy").setup({
     },
 
     {
-        "kevinhwang91/nvim-bqf",
-        dependencies = {
-            "junegunn/fzf",
-        },
+        "stevearc/quicker.nvim",
+        ft = "qf",
     },
+    -- {
+    --     "kevinhwang91/nvim-bqf",
+    --     dependencies = {
+    --         "junegunn/fzf",
+    --     },
+    -- },
 
     {
         "yorickpeterse/nvim-pqf",
@@ -264,7 +271,7 @@ require("lazy").setup({
     { "ziontee113/neo-minimap" },
     -- DAP plugins
     { "mfussenegger/nvim-dap" },
-    { "rolfst/nvim-dap-vscode-js", lazy = true },
+    { "rolfst/nvim-dap-vscode-js",         lazy = true },
     -- {
     --     "microsoft/vscode-js-debug",
     --     build = "npm install --legacy-peer-deps --ignore-scripts --production && npx gulp vsDebugServerBundle && mv dist out && ln -s "
@@ -343,7 +350,7 @@ require("lazy").setup({
     {
         "mrcjkb/rustaceanvim",
         version = "^6", -- Recommended
-        lazy = false, -- This plugin is already lazy
+        lazy = false,   -- This plugin is already lazy
     },
     {
         "mrcjkb/haskell-tools.nvim",
@@ -418,7 +425,7 @@ require("lazy").setup({
     {
         "saxon1964/neovim-tips",
         version = "*", -- Only update on tagged releases
-        lazy = true, -- Load only when keybinds are triggered
+        lazy = true,   -- Load only when keybinds are triggered
         dependencies = {
             "MunifTanjim/nui.nvim",
             -- OPTIONAL: Choose your preferred markdown renderer (or omit for raw markdown)
@@ -435,15 +442,15 @@ require("lazy").setup({
             bookmark_symbol = "🌟 ",
         },
         keys = {
-            { "<leader>nto", ":NeovimTips<CR>", desc = "Neovim tips" },
+            { "<leader>nto", ":NeovimTips<CR>",     desc = "Neovim tips" },
             {
                 "<leader>ntr",
                 ":NeovimTipsRandom<CR>",
                 desc = "Show random tip",
             },
             { "<leader>nte", ":NeovimTipsEdit<CR>", desc = "Edit your tips" },
-            { "<leader>nta", ":NeovimTipsAdd<CR>", desc = "Add your tip" },
-            { "<leader>ntp", ":NeovimTipsPdf<CR>", desc = "Open tips PDF" },
+            { "<leader>nta", ":NeovimTipsAdd<CR>",  desc = "Add your tip" },
+            { "<leader>ntp", ":NeovimTipsPdf<CR>",  desc = "Open tips PDF" },
         },
     },
     { "sotte/presenting.vim" },
